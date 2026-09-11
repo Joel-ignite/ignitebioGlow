79f17236bfb218c0b30c2a9dda4d172ad1ce6059
Joel-ignite <joel.g.williamson@gmail.com>
2026-09-11 10:09:02 -0500

    Add color sensor functionality to Joelcolorsensor.py

---
+++ Joelcolorsensor.py
@@ -0,0 +1,32 @@
+from pybricks.hubs import PrimeHub  # type: ignore[import-not-found]
+from pybricks.pupdevices import Motor  # type: ignore[import-not-found]
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop  # type: ignore[import-not-found]
+from pybricks.robotics import DriveBase  # type: ignore[import-not-found]
+from pybricks.tools import wait, StopWatch  # type: ignore[import-not-found]
+from pybricks.pupdevices import Motor, ColorSensor # type: ignore
+
+hub = PrimeHub()
+
+leftmotor = Motor(Port.F, Direction.COUNTERCLOCKWISE)
+rightmotor = Motor(Port.D)
+armleft = Motor(Port.B)
+armright = Motor(Port.E)
+ColorTabEye = ColorSensor(Port.A)
+Downeye = ColorSensor(Port.C)
+bot = DriveBase(leftmotor, rightmotor,wheel_diameter=430, axle_track=95)
+#adding this in case the code gets stuck in a loop, it will stop after 2 seconds and 4000 milliseconds of no movement
+#bot.settings(stalled_tolerances=(2, 4000)) 
+
+bot.use_gyro(True)
+#bot.settings(turn_rate=100, turn_acceleration=500)
+
+#bot.straight(100)
+hub.speaker.beep(200, 200)
+
+#Mission TabGREENSideRed
+while True:
+    # Checks if the detected color is blue
+    # Add the wait to make sure the tab is out of the sensor's slot to prvent a loop
+    if ColorTabEye.color() == Color.GREEN:
+        wait(1500)
+        bot.straight(2000)
