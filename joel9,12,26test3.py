8cf2d55cf61c0cc61b4e5626264b6dbf3f12d783
Joel-ignite <joel.g.williamson@gmail.com>
2026-09-12 10:06:52 -0500

    Set up PrimeHub and robot motors with sensors
    Initialize robot components and set up motors and sensors.

---
+++ joel9,12,26test3.py
@@ -0,0 +1,36 @@
+from pybricks.hubs import PrimeHub  # type: ignore[import-not-found]
+from pybricks.pupdevices import Motor  # type: ignore[import-not-found]
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop  # type: ignore[import-not-found]
+from pybricks.robotics import DriveBase  # type: ignore[import-not-found]
+from pybricks.tools import wait, StopWatch  # type: ignore[import-not-found]
+from pybricks.pupdevices import Motor, ColorSensor # type: ignore
+ 
+
+hub = PrimeHub()
+
+leftmotor = Motor(Port.F)
+rightmotor = Motor(Port.D)
+armleft = Motor(Port.B)
+armright = Motor(Port.E)
+ColorTabEye = ColorSensor(Port.A)
+Downeye = ColorSensor(Port.C)
+bot = DriveBase(leftmotor, rightmotor, wheel_diameter=43, axle_track=95)
+#adding this in case the code gets stuck in a loop, it will stop after 2 seconds and 4000 milliseconds of no movement
+#bot.settings(stalled_tolerances=(2, 4000)) 
+
+#bot.use_gyro(True)
+#bot.settings(turn_rate=100, turn_acceleration=500)
+#bot.end is the code that stops, not just the end of the code, it is a command that stops
+#the robot from moving and ends the program.
+#bot.straight(100)
+hub.speaker.play_notes(['C4/4', 'E4/4'], tempo=120)
+#Mission emergencyredside
+#while True:
+    # Add the wait to make sure the tab is out of the sensor's slot to prvent a loop
+    #if ColorTabEye.color() == Color.YELLOW:
+wait(1500)
+     
+bot.turn(-90)
+bot.straight(400)
+bot.stop ()
+#break
